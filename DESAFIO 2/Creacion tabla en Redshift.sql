8c2bb9648659fe4170e31ee895d969d42d763bfd
gortega1929 <gustavo.ortega1929@gmail.com>
2023-12-24 09:26:00 -0500

    'Commit con version final de DESAFIO 2 de curso coder DE'

--- DESAFIO 2/Creacion tabla en Redshift.sql
+++ DESAFIO 2/Creacion tabla en Redshift.sql
@@ -1,12 +1,14 @@
+-- DROP TABLE gustavo_ortega1929_coderhouse.stock_price_data;
+
 CREATE TABLE gustavo_ortega1929_coderhouse.stock_price_data (
-    id int PRIMARY KEY,
-    ticker VARCHAR(10) NOT NULL,
-    date DATE NOT NULL,
-    open_price DECIMAL,
-    high_price DECIMAL,
-    low_price DECIMAL,
-    close_price DECIMAL,
-    volume BIGINT
+    ticker VARCHAR(10),
+    "date" VARCHAR(10),
+    open_price FLOAT8,
+    high_price FLOAT8,
+    low_price FLOAT8,
+    close_price FLOAT8,
+    volume BIGINT,
+    load_date VARCHAR(20)
 );
 
 select * from gustavo_ortega1929_coderhouse.stock_price_data;
